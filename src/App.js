6abeb6e12818118ec480e4196263308f44e889f0
Luan Henrique <luanhgs0111@gmail.com>
2019-03-18 11:35:37 -0300

    Opa

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+
+import "./App.css";
+import Header from "./components/Header";
+import NewUser from "./components/NewUser";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cor: "white",
-      contador: 0
-    };
-  }
-
-  trocarCor = e => {
-    let contador = this.state.contador;
-    let cor = ++contador % 2 == 0 ? "red" : "blue";
-    this.setState({
-      cor: cor,
-      contador: contador
-    });
-  };
-
   render() {
-    const estilo = {
-      padding: 10,
-      background: this.state.cor
-    };
     return (
       <div>
-        <h1 style={estilo} onClick={this.trocarCor}>
-          {this.props.texto}
-        </h1>
-        <h2> Contador {this.state.contador} </h2>
+        <Header />
+        <NewUser />
       </div>
     );
   }
